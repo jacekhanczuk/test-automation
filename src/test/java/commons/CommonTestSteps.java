11f50afc081dbc38ee76d949c8f0775467acfb92
jhanczuk <jacekskrzynka@gmail.com>
2018-01-28 22:38:55 +0100

    load main page removed from commonTestSteps

--- src/test/java/commons/CommonTestSteps.java
+++ src/test/java/commons/CommonTestSteps.java
@@ -4,16 +4,10 @@ import org.openqa.selenium.WebDriver;
 import website.javascript.Javascript;
 
 public class CommonTestSteps {
-	private WebDriver driver;
 	private Javascript javascript;
 
 	public CommonTestSteps(WebDriver driver) {
-		this.driver = driver;
 		javascript = new Javascript(driver);
-	}
-
-	public void loadMainPage(String url) {
-		driver.get(url);
 	}
 
 	public void moveToFooterContent() {
